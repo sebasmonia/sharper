8265c2bc40c426ffb7d8e3536aa038c5b5deb598
Sebastián Monía <sebastian.monia@starz.com>
2020-07-22 17:02:19 -0600

    Add "dotnet run" support, handle some Windows quoting, async command changes

--- sharper.el
+++ sharper.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Sebastian Monia <smonia@outlook.com>
 ;; URL: https://github.com/sebasmonia/sharper
-;; Package-Requires: ((emacs "25"))
+;; Package-Requires: ((emacs "25") (transient "20200601"))
 ;; Version: 1.0
 ;; Keywords: maint tool
 
@@ -33,7 +33,7 @@
 
 (require 'transient)
 (require 'cl-lib)
-(require 'cl-extra)
+(require 'cl-extra) ;; for cl-some
 (require 'project)
 
 (defun sharper--message (text)
@@ -73,19 +73,25 @@
 ;; %t = TARGET
 ;; %o = OPTIONS
 ;; %s = SOLUTION
-;; %p = PROJECT
+;; %m = MS BUILD PROPERTIES (-p:Prop1=Val1 -p:Prop2=Val2 )
+;; %a = RUN SETTINGS (dotnet test) or APPLICATION ARGUMENTS (dotnet run)
+
 ;; %k = PACKAGE NAME
-;; %r = RUNTIME SETTINGS (dotnet test only)
-(defvar sharper--build-template "dotnet build %t %o %p" "Template for \"dotnet build\" invocations.")
-(defvar sharper--test-template "dotnet test %t %o %r" "Template for \"dotnet test\" invocations.")
+(defvar sharper--build-template "dotnet build %t %o %m" "Template for \"dotnet build\" invocations.")
+(defvar sharper--test-template "dotnet test %t %o %a" "Template for \"dotnet test\" invocations.")
 (defvar sharper--clean-template "dotnet clean %t %o" "Template for \"dotnet clean\" invocations.")
 (defvar sharper--publish-template "dotnet publish %t %o" "Template for \"dotnet publish\" invocations.")
-(defvar sharper--pack-template "dotnet pack %t %o %p" "Template for \"dotnet pack\" invocations.")
+(defvar sharper--pack-template "dotnet pack %t %o %m" "Template for \"dotnet pack\" invocations.")
+(defvar sharper--run-template "dotnet run --project %t %o %a" "Template for \"dotnet run\" invocations.")
 
-(defvar sharper--last-build nil "Last command used for a build")
-(defvar sharper--last-test nil "Last command used to run tests")
-(defvar sharper--last-publish nil "Last command used to run a publish")
-(defvar sharper--last-pack nil "Last command used to create a NuGet package")
+
+;; NOTE: if I start needing more than just default-dir + command I might as well create
+;; a struct that has directory, command, prop1, prop2 etc.
+(defvar sharper--last-build nil "A cons cell (directory . last command used for a build)")
+(defvar sharper--last-test nil "A cons cell (directory . last command used to run tests)")
+(defvar sharper--last-publish nil "A cons cell (directory . last command used to run a publish)")
+(defvar sharper--last-pack nil "A cons cell (directory . last command used to create a NuGet package)")
+(defvar sharper--last-run nil "A cons cell (directory . last command used for \"dotnet run\")")
 
 ;;------------------Main transient------------------------------------------------
 
@@ -97,6 +103,9 @@
   ["Run test"
    ("T" "new test run" sharper-transient-test)
    ("t" "repeat last test run" sharper--run-last-test)]
+  ["Run application"
+   ("R" "new application run" sharper-transient-run)
+   ("r" "repeat last application run" sharper--run-last-run)]
   ["Publish app"
    ("P" "new publish" sharper-transient-publish)
    ("p" "repeat last publish" sharper--run-last-publish)]
@@ -118,9 +127,10 @@
    (list (transient-args 'sharper-transient-build)))
   (transient-set)
   (if sharper--last-build
-      (progn
-        (sharper--log "Compilation command\n" sharper--last-build "\n")
-        (compile sharper--last-build))
+      (let ((default-directory (car sharper--last-build))
+            (command (cdr sharper--last-build)))
+        (sharper--log "Compilation command\n" command "\n")
+        (compile command))
     (sharper-transient-build)))
 
 (defun sharper--run-last-test (&optional transient-params)
@@ -130,9 +140,10 @@
   (transient-set)
   (sharper--log "Test command\n" sharper--last-test "\n")
   (if sharper--last-test
-      (progn
-        (sharper--log "Test command\n" sharper--last-test "\n")
-        (compile sharper--last-test))
+      (let ((default-directory (car sharper--last-test))
+            (command (cdr sharper--last-test)))
+        (sharper--log "Test command\n" command "\n")
+        (compile command))
     (sharper-transient-test)))
 
 (defun sharper--run-last-publish (&optional transient-params)
@@ -141,9 +152,11 @@
    (list (transient-args 'sharper-transient-publish)))
   (transient-set)
   (if sharper--last-publish
-      (progn
-        (sharper--log "Publish command\n" sharper--last-publish "\n")
-        (async-shell-command sharper--last-publish))
+      (let ((default-directory (car sharper--last-publish))
+            (command (cdr sharper--last-publish)))
+        (sharper--log "Publish command\n" command "\n")
+        (sharper--run-async-shell command "*dotnet publish*")
+        (pop-to-buffer "*dotnet publish*"))
     (sharper-transient-publish)))
 
 (defun sharper--run-last-pack (&optional transient-params)
@@ -152,11 +165,24 @@
    (list (transient-args 'sharper-transient-pack)))
   (transient-set)
   (if sharper--last-pack
-      (progn
-        (sharper--log "Pack command\n" sharper--last-pack "\n")
-        (compile sharper--last-pack))
+      (let ((default-directory (car sharper--last-pack))
+            (command (cdr sharper--last-pack)))
+        (sharper--log "Pack command\n" command "\n")
+        (compile command))
     (sharper-transient-pack)))
 
+(defun sharper--run-last-run (&optional transient-params)
+  "Run \"dotnet run\", ignore TRANSIENT-PARAMS, repeat last call via `sharper--last-run'."
+  (interactive
+   (list (transient-args 'sharper-transient-run)))
+  (transient-set)
+  (if sharper--last-run
+      (let ((default-directory (car sharper--last-run))
+            (command (cdr sharper--last-run)))
+        (sharper--log "Run command\n" command "\n")
+        (sharper--run-async-shell command "*dotnet run*")
+        (pop-to-buffer "*dotnet run*"))
+    (sharper-transient-run)))
 
 (defun sharper--version-info (&optional transient-params)
   "Run \"dotnet build\", ignore TRANSIENT-PARAMS, repeat last call via `sharper--last-pack'."
@@ -233,11 +259,28 @@
 
 ;;------------------dotnet common-------------------------------------------------
 
-(defun sharper--project-dir (&optional path)
-  "Get the project rootfrom optional PATH or `default-directory'."
+(defun sharper--project-root (&optional path)
+  "Get the project root from optional PATH or `default-directory'."
+  ;; TODO: maybe don't quote path when pulling the arg?
+  ;; It works on Linux, breaks on Windows...
+  ;; ALso "shell unquote" doesn't seem to be an possibility
+  (when (and (string= system-type "windows-nt")
+             path)
+    (setq path (substring path 1 -1)))
   (project-root (project-current nil
                                  (or path
                                      default-directory))))
+
+(defun sharper--path-directory (path)
+  "Get the directory name PATH.
+The only reason this is a function and not a plain call to `file-name-directory'
+is because of quoted path issues on Windows."
+  ;; TODO: maybe don't quote path when pulling the arg?
+  ;; It works on Linux, breaks on Windows...
+  ;; ALso "shell unquote" doesn't seem to be an possibility
+  (when (string= system-type "windows-nt")
+    (setq path (substring path 1 -1)))
+  (file-name-directory path))
 
 (defun sharper--filename-proj-or-sln-p (filename)
   "Return non-nil if FILENAME is a project or solution."
@@ -292,6 +335,14 @@
   :reader (lambda (_prompt _initial-input _history)
             (sharper--read-solution-or-project)))
 
+(define-infix-argument sharper--option-target-proj ()
+  :description "<PROJECT>"
+  :class 'transient-option
+  :shortarg "T"
+  :argument "<TARGET>="
+  :reader (lambda (_prompt _initial-input _history)
+            (sharper--read--project)))
+
 (define-infix-argument sharper--option-msbuild-params ()
   :description "<MSBuildProperties>"
   :class 'transient-option
@@ -299,6 +350,12 @@
   :argument "<MSBuildProperties>="
   :reader (lambda (_prompt _initial-input _history)
             (sharper--read-msbuild-properties)))
+
+(defun sharper--run-async-shell (command buffer-name)
+  "Call `async-shell-command' to run COMMAND using a buffer BUFFER-NAME."
+  (let ((le-buffer (generate-new-buffer (generate-new-buffer-name
+                                         buffer-name))))
+    (async-shell-command command le-buffer le-buffer)))
 
 ;;------------------dotnet build--------------------------------------------------
 
@@ -312,14 +369,14 @@
          (msbuild-props (sharper--get-argument "<MSBuildProperties>=" transient-params))
          ;; We want *compilation* to happen at the root directory
          ;; of the selected project/solution
-         (default-directory (sharper--project-dir target)))
+         (directory (sharper--project-root target)))
     (unless target ;; it is possible to build without a target :shrug:
       (sharper--message "No TARGET provided, will build in default directory."))
     (let ((command (format-spec sharper--build-template
                                 (format-spec-make ?t (or target "")
                                                   ?o (sharper--option-alist-to-string options)
-                                                  ?p (or msbuild-props "")))))
-      (setq sharper--last-build command)
+                                                  ?m (or msbuild-props "")))))
+      (setq sharper--last-build (cons directory command))
       (sharper--run-last-build))))
 
 (define-transient-command sharper-transient-build ()
@@ -351,19 +408,19 @@
   (transient-set)
   (let* ((target (sharper--get-target transient-params))
          (options (shaper--only-options transient-params))
-         (runtime-settings (sharper--get-argument "<RunSettings>=" transient-params))
+         (run-settings (sharper--get-argument "<RunSettings>=" transient-params))
          ;; We want *compilation* to happen at the root directory
          ;; of the selected project/solution
-         (default-directory (sharper--project-dir target)))
+         (directory (sharper--project-root target)))
     (unless target ;; it is possible to test without a target :shrug:
       (sharper--message "No TARGET provided, will run tests in default directory."))
     (let ((command (format-spec sharper--test-template
                                 (format-spec-make ?t (or target "")
                                                   ?o (sharper--option-alist-to-string options)
-                                                  ?r (if runtime-settings
+                                                  ?a (if run-settings
                                                          (concat "-- " runtime-settings)
                                                        "")))))
-      (setq sharper--last-test command)
+      (setq sharper--last-test (cons directory command))
       (sharper--run-last-test))))
 
 (define-infix-argument sharper--option-test-runsettings ()
@@ -414,14 +471,14 @@
          (options (shaper--only-options transient-params))
          ;; We want async-shell-command to happen at the root directory
          ;; of the selected project/solution
-         (default-directory (sharper--project-dir target)))
+         (default-directory (sharper--project-root target)))
     (unless target ;; it is possible to build without a target :shrug:
       (sharper--message "No TARGET provided, will run clean in default directory."))
     (let ((command (format-spec sharper--clean-template
                                 (format-spec-make ?t (or target "")
                                                   ?o (sharper--option-alist-to-string options)))))
       (sharper--log "Clean command\n" command "\n")
-      (async-shell-command command "*sharper - clean output*"))))
+      (sharper--run-async-shell command "*dotnet clean*"))))
 
 (define-transient-command sharper-transient-clean ()
   "dotnet clean menu"
@@ -450,13 +507,13 @@
          (options (shaper--only-options transient-params))
          ;; We want async-shell-command to happen at the root directory
          ;; of the selected project/solution
-         (default-directory (sharper--project-dir target)))
+         (directory (sharper--project-root target)))
     (unless target ;; it is possible to test without a target :shrug:
       (sharper--message "No TARGET provided, will run tests in default directory."))
     (let ((command (format-spec sharper--publish-template
                                 (format-spec-make ?t (or target "")
                                                   ?o (sharper--option-alist-to-string options)))))
-      (setq sharper--last-publish command)
+      (setq sharper--last-publish (cons directory command))
       (sharper--run-last-publish))))
 
 (define-transient-command sharper-transient-publish ()
@@ -496,14 +553,14 @@
          (msbuild-props (sharper--get-argument "<MSBuildProperties>=" transient-params))
          ;; We want *compilation* to happen at the root directory
          ;; of the selected project/solution
-         (default-directory (sharper--project-dir target)))
+         (directory (sharper--project-root target)))
     (unless target ;; it is possible to build without a target :shrug:
       (sharper--message "No TARGET provided, will pack in default directory."))
     (let ((command (format-spec sharper--pack-template
                                 (format-spec-make ?t (or target "")
                                                   ?o (sharper--option-alist-to-string options)
-                                                  ?p (or msbuild-props "")))))
-      (setq sharper--last-pack command)
+                                                  ?m (or msbuild-props "")))))
+      (setq sharper--last-pack (cons directory command))
       (sharper--run-last-pack))))
 
 (define-transient-command sharper-transient-pack ()
@@ -530,6 +587,65 @@
    ("p" "pack" sharper--pack)
    ("q" "quit" transient-quit-all)])
 
+;;------------------dotnet run----------------------------------------------------
+
+(defun sharper--run (&optional transient-params)
+  "Run \"dotnet run\" using TRANSIENT-PARAMS as arguments & options."
+  (interactive
+   (list (transient-args 'sharper-transient-run)))
+  (transient-set)
+  (let* ((target (sharper--get-target transient-params))
+         (options (shaper--only-options transient-params))
+         (app-args (sharper--get-argument "<ApplicationArguments>=" transient-params))
+         ;; For run we want this to execute in the same directory
+         ;; that the project is, where the .settings file is
+         (directory (sharper--path-directory target)))
+    (unless target ;; it is possible to run without a target :shrug:
+      (sharper--message "No TARGET provided, will run in default directory."))
+    (let ((command (format-spec sharper--run-template
+                                (format-spec-make ?t (or target "")
+                                                  ?o (sharper--option-alist-to-string options)
+                                                  ?a (if app-args
+                                                         (concat "-- " app-args)
+                                                       "")))))
+      (setq sharper--last-run (cons directory command))
+      (sharper--run-last-run))))
+
+(define-infix-argument sharper--option-run-application-arguments ()
+  :description "Application arguments"
+  :class 'transient-option
+  :shortarg "AA"
+  :argument "<ApplicationArguments>="
+  :reader (lambda (_prompt _initial-input _history)
+            (read-string "Application arguments: ")))
+
+;; dotnet run [-c|--configuration <CONFIGURATION>] [-f|--framework <FRAMEWORK>]
+;;     [--force] [--interactive] [--launch-profile <NAME>] [--no-build]
+;;     [--no-dependencies] [--no-launch-profile] [--no-restore]
+;;     [-p|--project <PATH>] [-r|--runtime <RUNTIME_IDENTIFIER>]
+;;     [-v|--verbosity <LEVEL>] [[--] [application arguments]]
+
+(define-transient-command sharper-transient-run ()
+  "dotnet run menu"
+  :value '("--configuration=Debug" "--verbosity=minimal")
+  ["Common Arguments"
+   (sharper--option-target-proj)
+   (sharper--option-run-application-arguments)
+   ("-c" "Configuration" "--configuration=")
+   ("-v" "Verbosity" "--verbosity=")]
+  ["Other Arguments"
+   ("-lp" "Launch profile" "--launch-profile=")
+   ("-f" "Force" "--force")
+   ("-w" "Framework" "--framework=")
+   ("-r" "Target runtime" "--runtime=")
+   ("-o" "Output" "--output=")
+   ("-nl" "No launch profile" "--no-launch-profile")
+   ("-nr" "No restore" "--no-restore")
+   ("-nb" "No build" "--no-build")
+   ("-nd" "No dependencies" "--no-dependencies")]
+  ["Actions"
+   ("r" "run" sharper--run)
+   ("q" "quit" transient-quit-all)])
 
 (provide 'sharper)
 ;;; sharper.el ends here
